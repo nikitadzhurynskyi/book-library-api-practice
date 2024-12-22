3223a0c1935107ed5c12e6a72d101ff810cdb4fc
Flamierdx <94560753+Flamierdx@users.noreply.github.com>
2024-12-22 15:04:24 +0000

    Initial commit

---
+++ src/main/java/com/kiyotaka/booklibraryapipractice/BookLibraryApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.kiyotaka.booklibraryapipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibraryApiPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookLibraryApiPracticeApplication.class, args);
+    }
+
+}
